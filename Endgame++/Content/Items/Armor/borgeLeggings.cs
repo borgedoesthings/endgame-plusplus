a708a70358b82642dbadc538ddfa67e1ec30447a
borge <thisisborge@gmail.com>
2024-09-25 18:24:37 -0400

    Update borgeLeggings.cs

--- Endgame++/Content/Items/Armor/borgeLeggings.cs
+++ Endgame++/Content/Items/Armor/borgeLeggings.cs
@@ -3,23 +3,23 @@ using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
 
-namespace ExampleMod.Content.Items.Armor
+namespace Endgame++.Content.Items.Armor
 {
 	// The AutoloadEquip attribute automatically attaches an equip texture to this item.
 	// Providing the EquipType.Legs value here will result in TML expecting a X_Legs.png file to be placed next to the item's main texture.
 	[AutoloadEquip(EquipType.Legs)]
-	public class ExampleLeggings : ModItem
+	public class borgeLeggings : ModItem
 	{
-		public static readonly int MoveSpeedBonus = 5;
+		public static readonly int MoveSpeedBonus = 15;
 
 		public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(MoveSpeedBonus);
 
 		public override void SetDefaults() {
 			Item.width = 18; // Width of the item
 			Item.height = 18; // Height of the item
-			Item.value = Item.sellPrice(gold: 1); // How many coins the item is worth
-			Item.rare = ItemRarityID.Green; // The rarity of the item
-			Item.defense = 5; // The amount of defense the item will give when equipped
+			Item.value = Item.sellPrice(platinum: 10, gold: 10); // How many coins the item is worth
+			Item.rare = ItemRarityID.Rainbow; // The rarity of the item
+			Item.defense = 45; // The amount of defense the item will give when equipped
 		}
 
 		public override void UpdateEquip(Player player) {
