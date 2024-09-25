6a8a50551f53471e0b58c6a36122dd45385af22e
borge <thisisborge@gmail.com>
2024-09-25 16:58:49 -0400

    Update borgeChest.cs

--- Endgame++/Content/Items/Armor/borgeChest.cs
+++ Endgame++/Content/Items/Armor/borgeChest.cs
@@ -18,9 +18,9 @@ namespace endgame.Content.Items.Armor
 		public override void SetDefaults() {
 			Item.width = 18; // Width of the item
 			Item.height = 18; // Height of the item
-			Item.value = Item.sellPrice(gold: 1); // How many coins the item is worth
-			Item.rare = ItemRarityID.Green; // The rarity of the item
-			Item.defense = 6; // The amount of defense the item will give when equipped
+			Item.value = Item.sellPrice(platinum:1, gold: 10); // How many coins the item is worth
+			Item.rare = ItemRarityID.Rainbow; // The rarity of the item
+			Item.defense = 60; // The amount of defense the item will give when equipped
 		}
 
 		public override void UpdateEquip(Player player) {
