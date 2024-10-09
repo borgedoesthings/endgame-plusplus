3f8f070f79c60d82e8e53a35371cf4e5f13f6e06
borge <thisisborge@gmail.com>
2024-10-09 12:23:01 -0400

    Update BOOMstick.cs
    PLEASE FUCKING WORK IM BEGGING YOU AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Endgame++/Content/Items/Weapons/BOOMstick.cs
+++ Endgame++/Content/Items/Weapons/BOOMstick.cs
@@ -16,7 +16,7 @@ namespace Endgame++.Content.Items.Weapons
 
 			Item.autoReuse = true;  // Whether or not you can hold click to automatically use it again.
 			Item.damage = 12; // Sets the item's damage. Note that projectiles shot by this weapon will use its and the used ammunition's damage added together.
-			Item.DamageType = DamageClass.Ranged; // What type of damage does this item affect?
+			Item.DamageType = ModContent.GetInstance<Demolitionist>(); //should HOPEFULLY work
 			Item.knockBack = 4f; // Sets the item's knockback. Note that projectiles shot by this weapon will use its and the used ammunition's knockback added together.
 			Item.noMelee = true; // So the item's animation doesn't do damage.
 			Item.rare = ItemRarityID.Yellow; // The color that the item's name will be in-game.
